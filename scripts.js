544c5ebe41324c368978608f674c5bfefe4260d3
GustaHideki <guyokoyama@hotmail.com>
2023-06-06 11:38:04 -0300

    fisrt commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let imagem = document.querySelector(".cafe")
+let circulo = document.querySelector(".circulo")
+
+function trocaImagem(endereco){
+    imagem.src= endereco
+    
+}
+
+function trocaCirculo(cor){
+    circulo.style.background= cor
+ 
+}
